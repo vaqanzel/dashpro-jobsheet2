097341f01da5bfd9ef299a167ecc830eb4e923ef
vaqanzel <valengrehasta@gmail.com>
2023-09-07 23:26:30 +0700

    simpan kode java ke github

---
+++ MyFirstJava28.java
@@ -0,0 +1,12 @@
+
+/**
+*MyFirstJava28
+ */
+ public class MyFirstJava28 {
+    public static void main (String[] args) {
+        
+        //Menampilkan output nama
+        System.out.println("Nama Saya Valentina Santi Grehasta");
+    }
+ }
+ 
